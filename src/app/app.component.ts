b61c93347c2bdf4306ddf8bd8543e383d40c2ab5
Marius <pulsemusic8@hotmail.com>
2025-01-15 11:12:38 +0000

    Added navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'benchmark-test';
-  intro: string = 'create a menu to display the test1 and test2 components';
+  title = "benchmark-test";
+  intro: string = "create a menu to display the test1 and test2 components";
 }
