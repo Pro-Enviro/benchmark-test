c03c11b1c30239b0498378ddd2023a7866f8e6bd
Marius <pulsemusic8@hotmail.com>
2025-01-15 12:36:54 +0000

    Adding style to products page, custom classes to stock, custom font

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -6,22 +6,8 @@ import { Product } from "../types/product.interface";
   selector: "app-product-card",
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div>
-      <h3>{{ product.title }}</h3>
-    </div>
-  `,
-  styles: [
-    `
-      .product-card {
-        border: 1px solid #ddd;
-        padding: 15px;
-        margin: 10px;
-        border-radius: 8px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-      }
-    `,
-  ],
+  templateUrl: "./product-card.component.html",
+  styleUrl: "./product-card.component.css",
 })
 export class ProductCardComponent {
   @Input() product!: Product;
