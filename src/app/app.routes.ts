37e4c9ce581db4a1e6c686180e50773c4c45a0d8
Marius <pulsemusic8@hotmail.com>
2025-01-15 13:09:37 +0000

    Adding product-details component, and future Challenges page that i encountered

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
 import { Test1Component } from "./test1/test1.component";
 import { Test2Component } from "./test2/test2.component";
+import { ChallengesComponent } from "./challenges/challenges.component";
 
 export const routes: Routes = [
   { path: "test1", component: Test1Component },
   { path: "test2", component: Test2Component },
+  { path: "challenges", component: ChallengesComponent },
   { path: "", redirectTo: "/test2", pathMatch: "full" },
 ];
