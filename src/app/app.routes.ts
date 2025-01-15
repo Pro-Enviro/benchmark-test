0186821928f0ee4a5fb17a8f0779f3b0d0b76aa8
Marius <pulsemusic8@hotmail.com>
2025-01-15 13:15:01 +0000

    Adding individual product page, styles later, for now figuring out the layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from "@angular/router";
 import { Test1Component } from "./test1/test1.component";
 import { Test2Component } from "./test2/test2.component";
 import { ChallengesComponent } from "./challenges/challenges.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 export const routes: Routes = [
   { path: "test1", component: Test1Component },
   { path: "test2", component: Test2Component },
+  { path: "product/:id", component: ProductDetailsComponent },
   { path: "challenges", component: ChallengesComponent },
   { path: "", redirectTo: "/test2", pathMatch: "full" },
 ];
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-product-details',
-  imports: [],
-  templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  selector: "app-product-details",
+  imports: [RouterLink, CommonModule],
+  templateUrl: "./product-details.component.html",
+  styleUrl: "./product-details.component.css",
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: any;
+  currentImage: string = "";
 
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const id = params["id"];
+      this.http
+        .get<any>(`https://dummyjson.com/products/${id}`)
+        .subscribe((data) => {
+          this.product = data;
+          this.currentImage = data.images[0];
+        });
+    });
+  }
 }
