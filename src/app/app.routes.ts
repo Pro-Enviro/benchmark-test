4a37b0e83f0f89be61501e1a99ab24b20b7b5469
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-16 16:00:02 +0000

    Add home path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { Test1Component } from './test1/test1.component'
+import { Test2Component } from './test2/test2.component'
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test-1', component: Test1Component },
+  { path: 'test-2', component: Test2Component },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
