6685aa87b2ea2c4001bb11c4dbb592ca1ea7c816
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-01-16 12:53:05 +0000

    created menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'benchmark-test';
-  intro: string = 'create a menu to display the test1 and test2 components';
+  title = "benchmark-test";
+  intro: string = "create a menu to display the test1 and test2 components";
+
+  isMenuOpen: boolean = false;
+
+  onMenuBtnClick(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
